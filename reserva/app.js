63d26b54f22e22fb429758ae7d97fb4e59a40fb0
adm <adm@labsoft.local>
2024-04-17 17:35:51 -0300

    faz funcionar sem Docker

--- reserva/app.js
+++ reserva/app.js
@@ -4,7 +4,15 @@ const port = 3000
 const {read, write, init} = require('./utils')
 const axios = require('axios')
 
+
+var bodyParser = require('body-parser')
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+
 init()
+
 
 app.get('/', (req, res) => {
   res.sendFile('index.html', {root: __dirname })
@@ -12,6 +20,7 @@ app.get('/', (req, res) => {
 
 app.post('/', (req, res) => {
     const data = read()
+    console.log(req.body)
     data[req.body.numeroAssento] = 'ocupado'
     console.log('post')
     write(data)
--- visualiza/app.js
+++ visualiza/app.js
@@ -3,6 +3,12 @@ const app = express()
 const port = 3001
 const {read, write, init} = require('./utils')
 
+var bodyParser = require('body-parser')
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+
 init()
 
 app.get('/', (req, res) => {
